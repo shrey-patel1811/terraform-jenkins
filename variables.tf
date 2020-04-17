4faead33252fbcabb6b61f746b0d11a0b4a77f89
Shrey Patel <shrey.patel7372@gmail.com>
2020-04-18 01:43:20 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,7 +3,7 @@ variable "instance_count" {
 }
 variable "key_name" {
   description = "Private key name to use with instance"
-  default     = "shrKey"
+  default     = "shrkey"
 }
 
 variable "instance_type" {
