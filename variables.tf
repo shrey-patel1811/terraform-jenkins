df7904a21031edb743e0b0a4d6125e17eb6ef39d
Shrey Patel <shrey.patel7372@gmail.com>
2020-04-18 01:10:27 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,6 +2,13 @@ variable "instance_count" {
   default = 1
 }
 
+variable "access_key" {
+  default = "AKIAQTUGGBQ5AURAFR77"
+}
+
+variable "secret_key" {
+  default = "+jM+vooqKHp5XGbWTK5NSm99TxbiFYy4AvCKnlwc"
+}
 variable "instance_type" {
   description = "AWS instance type"
   default     = "t2.micro"
