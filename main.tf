b72dce89758d0864221fde454bfe46966cf2afba
Shrey Patel <shrey.patel7372@gmail.com>
2020-04-18 01:54:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ provider "aws" {
 resource "aws_instance" "default" {
   ami                    = var.ami
   count                  = var.instance_count
-  key_name               = var.key_name
+ # key_name               = var.key_name
   vpc_security_group_ids = [aws_security_group.default.id]
   source_dest_check      = false
   instance_type          = var.instance_type
