c93df271cc8729ffaa61cb720067a499faab6845
Shrey Patel <shrey.patel7372@gmail.com>
2020-04-17 23:42:08 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,18 @@
+variable "instance_count" {
+  default = 1
+}
+
+variable "key_name" {
+  description = "Private key name to use with instance"
+  default     = "terraform"
+}
+
+variable "instance_type" {
+  description = "AWS instance type"
+  default     = "t3.small"
+}
+
+variable "ami" {
+  description = "Base AMI to launch the instances"
+  default = "ami-2757f631"
+}
